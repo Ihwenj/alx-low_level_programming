8efd3ef99886d8e2c98d90ce06ae2531bc08da23
Ihwenj <ihwenj@gmail.com>
2022-03-24 10:40:57 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+printf("the size of char is : %d byte(s)\n", sizeof(char));
+printf("the size of integer is : %d byte(s)\n", sizeof(int));                                                             printf("the size of double is : %d byte(s)\n", sizeof(double));                                                           printf("the size of float is : %d byte(s)\n", sizeof(float));
+return (0);
+}
