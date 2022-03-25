ecef4489a9f604b93fa6dd7873fe6dbf6aaca552
Ihwenj <ihwenj@gmail.com>
2022-03-25 09:37:30 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 /**
- *main -entry point
- *print function for all the size of typs in the system
+ * main -entry point
  *
- * Return: 0 when sucessful
+ * Return: 0 on succesfull
  */
 int main(void)
 {
-printf("the size of char is: %d byte(s)\n", sizeof(char));
-printf("the size of integer is: %d byte(s)\n", sizeof(int));
-printf("the size of long integer is: %d byte(s)\n", sizeof(long int));
-printf("the size of unsigned long integer is: %d byte(s)\n", sizeof(unsigned long int));
-printf("the size of long long integer is: %d byte(s)\n", sizeof(long long int));
-printf("the size of unsigned lng long integer is: %d byte(s)\n", sizeof(unsigned long long int));
-
+printf("the size of a char: %d byte(s)\n", sizeof(char));
+printf("the size of int: %d bytes(s)\n", sizeof(int));
+printf("the size of long int: %d byte(s)\n", sizeof(long int));
+printf("the size of float: %d byte(s)\n", sizeof(float));
+return (0);
 }
