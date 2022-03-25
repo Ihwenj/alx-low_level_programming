54d84ec09aab1e6898fa8b145e3d4a9029195598
Ihwenj <ihwenj@gmail.com>
2022-03-25 09:58:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,8 @@
 int main(void)
 {
 printf("the size of a char: %d byte(s)\n", sizeof(char));
-printf("the size of int: %d bytes(s)\n", sizeof(int));
-printf("the size of long int: %d byte(s)\n", sizeof(long int));
-printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("the size of int: %d bytes(s)\n", sizeof(int))
 printf("the size of float: %d byte(s)\n", sizeof(float));
+printf("the size of double: %d byte(s)\n", sizeof(double));
 return (0);
 }
