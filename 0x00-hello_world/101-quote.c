70cf074bd7b776edd8d2788153be0ad1c0a7f9ae
Ihwenj <ihwenj@gmail.com>
2022-03-26 07:56:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,21 +2,12 @@
 
 #include <unistd.h>
 
-
-
 /**
+ *main - print quote
+ *Description: prints Dora's quote
  *
- *  * main - print quote
- *
- *   *
- *
- *    * Description: prints Dora's quote
- *
- *     *
- *
- *      * Return: Always 1 (Success)
- *
- *       */
+ *Return: Always 1 (Success)
+ */
 
 
 
@@ -24,8 +15,8 @@ int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
+return (1);
 
 }
