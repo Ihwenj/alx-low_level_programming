4a913087c5669edf1a738d6e9afa77cb9fba7ec9
Ihwenj <ihwenj@gmail.com>
2022-03-26 07:43:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  *
  *Return: 1 if successful
  */
-int main(void)
+int main(void);
 {
-fprintf(stderr, "and that piece of art is useful %c - Dora Korpar,
-	       	2015-10-19\n", '"');
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
 }
