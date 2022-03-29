ac69acc30f0ba80c79137254b3e568f1962667d5
Ihwenj <ihwenj@gmail.com>
2022-03-29 02:33:49 -0700

    _putchar(_);

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
 /**
  *main - prints _putchar
@@ -8,14 +8,16 @@
  * Return: 0 (Success)
  */
 int main(void)
-{
-char text[9] = "_putchar";
-int i = 0;
 
-for (i = 0; i < 8; i++)
 {
-_putchar(text[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
